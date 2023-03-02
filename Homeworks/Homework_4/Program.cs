d65c760b47f27b8c9423c55ecf5caa99d1468031
Aleksandr Elovikov <aleksandrelovikov@mail.ru>
2023-03-02 15:57:53 +0100

    2.3.23

---
+++ Homeworks/Homework_4/Program.cs
@@ -0,0 +1,114 @@
+﻿// Урок 4. Функции
+// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+
+// 2, 4 -> 16
+
+
+// int Prompt(string message)
+// {
+//     System.Console.Write(message);
+//     string readInput = System.Console.ReadLine();
+//     int result = int.Parse(readInput);
+//     return result;
+// }
+
+// int Power(int powerBase, int exponent)
+// {
+//     int power = 1;
+//     for (int i = 0; i < exponent; i++)
+//     {
+//         power *= powerBase;
+//     }
+//     return power;
+// }
+
+// bool ValidateExponent(int exponent)
+// {
+//     if(exponent < 0)
+//     {
+//         System.Console.WriteLine("не должен быть < 0");
+//         return false;
+//     }
+//     return true;
+// }
+
+// int powerBase = Prompt("Enter the base for the power");
+// int exponent = Prompt("Enter the exponent for the power");
+// if (ValidateExponent(exponent))
+
+// {
+//     System.Console.WriteLine($" Число {powerBase} ^ {exponent} равно {Power(powerBase, exponent)}");
+// }
+
+
+// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+
+// 82 -> 10
+
+// 9012 -> 12
+
+// int Prompt(string message)
+// {
+//     System.Console.Write(message);
+//     string readInput = System.Console.ReadLine();
+//     int result = int.Parse(readInput);
+//     return result;
+// }
+
+// int SumAllDigit(int number)
+// {
+//     int result = 0;
+//     while (number > 0)
+//     {
+//         result += number % 10;
+//         number = number / 10;
+
+//     }
+//     return result;
+
+// }
+
+// int number = Prompt("Enter the number");
+// System.Console.WriteLine($"SumAllDigit{number} = {SumAllDigit(number)}");
+
+
+
+// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+// 6, 1, 33 -> [6, 1, 33]
+
+// int[] CreateRandomArray(int size, int minValue, int maxValue)
+// {
+//     int[] array = new int[size];
+//     for(int i = 0;i < size; i++)
+//     {
+//         array[i] = new Random().Next(minValue, maxValue + 1); 
+//     }
+//     return array;
+// }
+
+// void ShowArray (int[] array)
+// {
+//     for(int i = 0;i < array.Length; i++)
+//     {
+//         Console.Write(array[i] + " ");
+//     }
+//     Console.WriteLine();
+// }
+
+// Console.WriteLine("Input array size");
+// int size = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Input minimal value of array element");
+// int minValue = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Input max value of array element");
+// int maxValue = Convert.ToInt32(Console.ReadLine());
+
+// int[] myArray = CreateRandomArray(size, minValue, maxValue);
+
+// ShowArray(CreateRandomArray(size, minValue, maxValue));
